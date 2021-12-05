d6cd6a2bac4f720e6c704572ed64f56fe9c3536d
VladislavKarol <vlad230899@gmail.com>
2021-12-05 15:43:05 +0300

    feat: add cors/bodyParser

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import express from "express";
 import logger from "morgan";
 import * as path from "path";
 
+import cors from "cors";
+import bodyParser from "body-parser";
+
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
 // Routes
 import { index } from "./routes/index";
 // Create Express server
 export const app = express();
+
+app.use(cors());
+app.options("*", cors());
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
